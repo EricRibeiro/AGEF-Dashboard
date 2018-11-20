f40b4c4acd042e4dcb013a407f639ae078a0bbc0
Eric Ribeiro <ericribeiro@outlook.com.br>
2018-11-20 05:34:15 -0200

    Implementação da interface finalizada

---
+++ src/config/socket.io.config.ts
@@ -0,0 +1,6 @@
+import { SocketIoConfig } from 'ng-socket-io';
+
+export const SOCKET_IO_CONFIG: SocketIoConfig = {
+    url: 'https://floating-gorge-33344.herokuapp.com',
+    options: {}
+};
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Chart } from 'chart.js';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { Socket } from 'ng-socket-io';
+import * as palette from 'google-palette';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,62 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('canvasPizza') canvasPizza;
+
+  graficoPizza: any;
+
+  constructor(
+    public navCtrl: NavController,
+    public socket: Socket) {
+
+    this.getMessages().subscribe(message => {
+      console.log(message);
+    });
 
   }
+
+  ionViewDidLoad() {
+    let lojas: any[] = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth"];
+    let vendas = [12, 19, 3, 5, 2, 3];
+
+    this.exibirGrafico(lojas, vendas);
+  }
+
+  ionViewWillEnter() {
+    this.socket.connect();
+  }
+
+  ionViewWillLeave() {
+    this.socket.disconnect();
+  }
+
+  public exibirGrafico(lojas: string[], vendas: number[]): void {
+    Chart.defaults.global.legend.display = false;
+
+    this.graficoPizza = new Chart(this.canvasPizza.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: lojas,
+        datasets: [{
+          data: vendas,
+          backgroundColor: palette('cb-Set3', vendas.length).map(function (hex) {
+            return '#' + hex;
+          })
+        }],
+      },
+
+    });
+  }
+
+  public getMessages(): Observable<any> {
+    let observable = new Observable(observer => {
+      this.socket.on('message', (data) => {
+        observer.next(data);
+      });
+    })
+
+    return observable;
+  }
+
 
 }
